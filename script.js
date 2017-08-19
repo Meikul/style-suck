5419204b3ded82816aa418c294b2c49f8f14f9ba
Michael Harris <Meikul@users.noreply.github.com>
2017-08-18 18:04:56 -0600

    getting started can clean html, starting on css selectors

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 
-document.querySelector('input[type=button]').onlick = translate;
 
 function translate(){
   var input = document.getElementById('input').value;
-  console.log(getTags(input));
+  styleSuck(getTags(input));
 }
 
 function getTags(input){
@@ -27,22 +26,88 @@ var tags = [],
 }
 
 function styleSuck(tags){
+
   var outhtml = '',
       outstyle = '';
 
-  tags.forEach(function(str){
-    getStyleAttr(str);
+  tags.forEach(function(tag){
+    var i = getStyleIndex(tag);
+    if(i) outhtml += clipOut(tag, i[0], i[1]);
+    else outhtml += tag;
+    if(tag[1] != '/'){
+      var selector = findSelector(tag, tags);
+    }
+    var re = new RegExp('<'+tagType+' (.+)>');
+    console.log(tag.match(re));
+    if(tag.match(re)) console.log(tag.match(re)[1]);
   });
+
+  console.log(outhtml);
+}
+
+function findSelector(tag, tags){
+  var id = classList = nthChild = tagType = attr = undefined;
+
+  var idInd = tag.indexOf('id=');
+  var classInd = tag.indexOf('class=');
+
+  if(idInd>-1){
+    id = '#'+clipAttr(tag, idInd);
+    return id;
+  }
+
+  if(classInd>-1){
+    classList = clipAttr(tag, classInd).split(' ').join('.');
+  }
+
+  // Tag Type
+  tagType = tag.match(/<(\S+)\s/);
+  tagType = tagType ? tagType[1] : undefined;
+
+  // Attributes
+  var re = new RegExp('<'+tagType+' (.)>');
+  tag.match(re);
+}
+
+function clipOut(tag, i, j){
+  return tag.substring(0, i-1) + tag.substring(j+1);
+}
+
+function clipAttr(tag, i){
+  var openQuote = tag.indexOf('"', i);
+  if(openQuote == -1){
+    openQuote = tag.indexOf("'", i);
+    closeQuote = tag.indexOf("'", openQuote+1);
+  }
+  else closeQuote = tag.indexOf('"', openQuote+1);
+  return tag.substring(openQuote+1, closeQuote);
+}
+
+function getStyleIndex(tag){
+  var i = tag.indexOf('style='),
+      j = i;
+  if(i > -1){
+    if(tag[i+6]== "'") j = tag.indexOf("'", i+7);
+    else if(tag[i+6] == '"') j = tag.indexOf('"', i+7);
+    return [i, j];
+  }
+  return false;
 }
 
 function getStyleAttr(str){
+  // (["'])(?:(?=(\\?))\2.)*?\1
   var i = str.indexOf('style='),
       j = i;
   if(i > -1){
-    console.log(i);
     if(str[i+6]== "'") j = str.indexOf("'", i+7);
     else if(str[i+6] == '"') j = str.indexOf('"', i+7);
-    return str.substring(i, j);
+    return str.substring(i+7, j-1);
   }
   return '';
 }
+
+
+
+window.onload = function(){
+  document.getElementById('submit').onclick = translate;
+};
